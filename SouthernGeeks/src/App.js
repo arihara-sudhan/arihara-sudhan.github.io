018edfbc0f906a6e40ec5a46384aa1a576759f0a
Southern Boy <100225369+arihara-sudhan@users.noreply.github.com>
2024-06-28 17:13:05 +0530

    Update App.js

--- SouthernGeeks/src/App.js
+++ SouthernGeeks/src/App.js
@@ -7,11 +7,11 @@ function App() {
     <div className="App">
       
       <img src="https://arihara-sudhan.github.io/blog/img/logo.png" alt="" id="logo"/>
-      <h1>SOUTHERN GEEKS</h1>
+      <h1>ARI LEARNS</h1>
       {JSONDATA.map(task => (
         <div className='TASK'>
           <div className='CHRONA'>
-            <span>(DAY {task.day})-----|||||||||||----({task.date})</span>
+            <span>DAY {task.day} - {task.date}</span>
           </div>
           <div className='TASKS'>
             {task.topics.map((topic, idx) => (
