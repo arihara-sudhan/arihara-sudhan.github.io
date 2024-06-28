25961deb17508f7f9daccefc2e2aad933a1e4074
Southern Boy <100225369+arihara-sudhan@users.noreply.github.com>
2024-06-28 17:31:10 +0530

    Update App.js

--- SouthernGeeks/src/App.js
+++ SouthernGeeks/src/App.js
@@ -4,23 +4,25 @@ import JSONDATA from "./tasks.json";
 
 function App() {
   return (
-    <div className="App">
-      <img src="https://arihara-sudhan.github.io/blog/img/logo.png" alt="" id="logo"/>
-      <h1>ARI LEARNS</h1>
-      {JSONDATA.map(task => (
-        <div className='TASK'>
-          <div className='CHRONA'>
-            <span>DAY {task.day} - {task.date}</span>
-          </div>
-          <div className='TASKS'>
-            {task.topics.map((topic, idx) => (
-              <p>{idx+1}. {topic}<a href={task.reference[idx]}>
-                 <span className='refer'>REFERENCE</span></a></p>
-            ))}
-          </div>
-          </div>
-      ))}
-    </div>
+    <center>
+      <div className="App">
+        <img src="https://arihara-sudhan.github.io/blog/img/logo.png" alt="" id="logo"/>
+        <h1>ARI LEARNS</h1>
+        {JSONDATA.map(task => (
+          <div className='TASK'>
+            <div className='CHRONA'>
+              <span>DAY {task.day} - {task.date}</span>
+            </div>
+            <div className='TASKS'>
+              {task.topics.map((topic, idx) => (
+                <p>{idx+1}. {topic}<a href={task.reference[idx]}>
+                   <span className='refer'>REFERENCE</span></a></p>
+              ))}
+            </div>
+            </div>
+        ))}
+      </div>
+    </center>
   );
 }
 
